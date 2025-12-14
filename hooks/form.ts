6dd72d37a1bbde027faafbc7152825be82ba7854
marquesmaycon <mayconmarquesh@gmail.com>
2025-12-13 23:48:23 -0300

    select-field component

--- hooks/form.ts
+++ hooks/form.ts
@@ -3,6 +3,7 @@ import { lazy } from "react"
 
 const InputField = lazy(() => import("../components/form/input-field"))
 const TextareaField = lazy(() => import("../components/form/textarea-field"))
+const SelectField = lazy(() => import("../components/form/select-field"))
 const SubmitButton = lazy(() => import("../components/form/submit-button"))
 
 import { fieldContext, formContext } from "./form-context"
@@ -10,7 +11,8 @@ import { fieldContext, formContext } from "./form-context"
 export const { useAppForm, withForm, withFieldGroup } = createFormHook({
   fieldComponents: {
     InputField,
-    TextareaField
+    TextareaField,
+    SelectField
   },
   formComponents: {
     SubmitButton
