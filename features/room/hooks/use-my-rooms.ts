51f1cd94ba10e919bcdfe114967b57c2a0c23f58
marquesmaycon <mayconmarquesh@gmail.com>
2025-12-20 20:32:06 -0300

    auth on my rooms

--- features/room/hooks/use-my-rooms.ts
+++ features/room/hooks/use-my-rooms.ts
@@ -7,12 +7,15 @@ export const myRoomsQueryOptions = (headers?: ReadonlyHeaders) =>
   queryOptions({
     queryKey: ["my-rooms"],
     queryFn: async () => {
-      const res = await client.api.rooms["my-rooms"].$get({ header: headers })
+      const res = await client.api.rooms["my-rooms"].$get(
+        {},
+        {
+          headers: Object.fromEntries(headers?.entries() ?? [])
+        }
+      )
       const { rooms } = await res.json()
       return rooms
     }
   })
 
-export const useMyRooms = () => {
-  return useQuery(myRoomsQueryOptions())
-}
+export const useMyRooms = (headers?: ReadonlyHeaders) => useQuery(myRoomsQueryOptions(headers))
