53e0e78879ca3f2e2517d2d248229de0c7f544fd
marquesmaycon <mayconmarquesh@gmail.com>
2025-12-20 21:37:53 -0300

    react query dev tools

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 import { ThemeProvider } from "@/components/theme-provider"
 import { getQueryClient } from "@/lib/tanstack-query"
@@ -13,6 +14,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
         {children}
       </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
